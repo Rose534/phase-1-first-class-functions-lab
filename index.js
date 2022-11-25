10ce05443b8b867ed6065cecb8943bfeb447f267
rose534 <wambuirose534@gmail.com>
2022-11-25 22:59:40 +0300

    first-class-functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const drivers = ['rose', 'peter', 'sam', 'jason']
+const returnFirstTwoDrivers = (drivers) => {
+  const newArray = drivers.slice(0, 2);
+  return (newArray);
+}
+const returnLastTwoDrivers = (drivers) => {
+  const last = drivers.slice(-2);
+  return (last);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (ran) => 
+{
+  return () => ran * 5
+}
+const fareDoubler = (dis) => {
+  return dis * 2;
+
+}
+const fareTripler = (far) => {
+  return far * 3;
+}
+const selectDifferentDrivers = (drivers, ile) => {
+  return ile(drivers);
+}
